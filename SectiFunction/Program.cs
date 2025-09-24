9d2b6f055e3cc51439eb4cfdcd6fd9020ee09a86
Ladislav Jaromír Pleštil <ladaplestil09@seznam.cz>
2025-09-24 09:25:32 +0200

    vibecoded SectiFunction

---
+++ SectiFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(sectipico(1, 2));
+    }
+
+    private static float secti(float x, float y)
+    {
+        return x + y;
+    }
+
+    private static Func<float, float, float> sectipico => secti;
+}
